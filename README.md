ed1d0ef4b567da72a5fdbe92728281eb51b933a9
BernardinoR <rafaelbsilveiira@gmail.com>
2025-02-28 20:56:19 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Rebalanceador
-Puxar dados de corretoras em excel e formatalas 
+nvm l# Rebalanceador
+Puxar dados de corretoras em excel e formatalas
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-function Home() { 
-    return <h1>Bora fazer um software legal hehe </h1>
+function Home() {
+  return <h1>Bora fazer um software legal hehe </h1>;
 }
 export default Home;
