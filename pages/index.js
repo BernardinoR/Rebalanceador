e3b07717697ab1347e90800f926b1891b622c042
BernardinoR <rafaelbsilveiira@gmail.com>
2025-03-02 17:05:46 +0000

    Adicionei texto em pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 function Home() {
-  return <h1>Bora fazer um software legal hehe </h1>;
+  return (
+    <h1>Bora fazer um software legal hehe e também eu amo a tayane bundinha</h1>
+  );
 }
 export default Home;
